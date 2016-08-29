fc4c24ca3d5bb0f42d8b70c797482f2931be5a16
Keiho Sakapon <saka_pon@live.jp>
2016-08-29 18:59:03 +0900

    Add test

--- Harmonia/UnitTest/SortHelperTest.cs
+++ Harmonia/UnitTest/SortHelperTest.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using Harmonia;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
@@ -16,10 +17,26 @@ namespace UnitTest
         }
 
         [TestMethod]
+        public void MergeSort_2()
+        {
+            var expected = Enumerable.Range(0, 1000000).ToArray();
+            var target = expected.Reverse().ToArray();
+            CollectionAssert.AreEqual(expected, target.MergeSort());
+        }
+
+        [TestMethod]
         public void BubbleSort_1()
         {
             var target = new[] { 3, 1, 5, 4, 2 };
             var expected = new[] { 1, 2, 3, 4, 5 };
+            CollectionAssert.AreEqual(expected, target.BubbleSort());
+        }
+
+        [TestMethod]
+        public void BubbleSort_2()
+        {
+            var expected = Enumerable.Range(0, 10000).ToArray();
+            var target = expected.Reverse().ToArray();
             CollectionAssert.AreEqual(expected, target.BubbleSort());
         }
     }

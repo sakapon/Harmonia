4883e38da061ecf4c589361b099485c0800c72a1
Keiho Sakapon <saka_pon@live.jp>
2016-08-29 18:31:51 +0900

    Add test

--- Harmonia/UnitTest/SortHelperTest.cs
+++ Harmonia/UnitTest/SortHelperTest.cs
@@ -8,6 +8,14 @@ namespace UnitTest
     public class SortHelperTest
     {
         [TestMethod]
+        public void MergeSort_1()
+        {
+            var target = new[] { 3, 1, 5, 4, 2 };
+            var expected = new[] { 1, 2, 3, 4, 5 };
+            CollectionAssert.AreEqual(expected, target.MergeSort());
+        }
+
+        [TestMethod]
         public void BubbleSort_1()
         {
             var target = new[] { 3, 1, 5, 4, 2 };

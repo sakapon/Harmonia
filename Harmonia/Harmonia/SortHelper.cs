74f5e836f82cd2f950c26fa6f20f6b8247c34e74
Keiho Sakapon <git@saka-pon.net>
2016-09-20 16:05:47 +0900

    Move members

--- Harmonia/Harmonia/SortHelper.cs
+++ Harmonia/Harmonia/SortHelper.cs
@@ -65,50 +65,6 @@ namespace Harmonia
             }
             return source;
         }
-
-        /// <summary>
-        /// 要素をキーに従って昇順に並べ替えます。
-        /// バブル ソートを使用します。
-        /// </summary>
-        /// <typeparam name="TSource">要素の型。</typeparam>
-        /// <typeparam name="TKey">キーの型。</typeparam>
-        /// <param name="source">並べ替える配列。</param>
-        /// <param name="keySelector">キーを抽出する関数。</param>
-        /// <returns>並べ替えられた配列。</returns>
-        public static TSource[] BubbleSort<TSource, TKey>(this TSource[] source, Func<TSource, TKey> keySelector) where TKey : IComparable<TKey>
-        {
-            if (keySelector == null) throw new ArgumentNullException(nameof(keySelector));
-
-            var keyed = source.Select(e => new KeyedObject<TSource, TKey>(e, keySelector(e))).ToArray();
-            BubbleSort(keyed);
-            return keyed.Select(_ => _.Target).ToArray();
-        }
-
-        /// <summary>
-        /// 指定された配列自身をバブル ソートで並べ替えます。
-        /// </summary>
-        /// <typeparam name="TSource">要素の型。</typeparam>
-        /// <param name="source">並べ替える配列。</param>
-        /// <returns>並べ替えられた配列。参照は <paramref name="source"/> と同じ。</returns>
-        public static TSource[] BubbleSort<TSource>(this TSource[] source) where TSource : IComparable<TSource>
-        {
-            TSource e;
-
-            for (var i = 0; i < source.Length; i++)
-            {
-                for (var j = source.Length - 1; i < j; j--)
-                {
-                    // if target[j - 1] > target[j],
-                    if (source[j - 1].CompareTo(source[j]) > 0)
-                    {
-                        e = source[j - 1];
-                        source[j - 1] = source[j];
-                        source[j] = e;
-                    }
-                }
-            }
-            return source;
-        }
     }
 
     struct KeyedObject<TSource, TKey> : IComparable<KeyedObject<TSource, TKey>> where TKey : IComparable<TKey>
--- Harmonia/UnitTest/SortHelperTest.cs
+++ Harmonia/UnitTest/SortHelperTest.cs
@@ -35,33 +35,5 @@ namespace UnitTest
             foreach (var i in actual)
                 Console.WriteLine(i);
         }
-
-        [TestMethod]
-        public void BubbleSort_1()
-        {
-            var expected = new[] { 1, 2, 3, 4, 5 };
-            CollectionAssert.AreEqual(expected, new[] { 1, 2, 3, 4, 5 }.BubbleSort());
-            CollectionAssert.AreEqual(expected, new[] { 5, 4, 3, 2, 1 }.BubbleSort());
-            CollectionAssert.AreEqual(expected, new[] { 4, 2, 5, 1, 3 }.BubbleSort());
-            CollectionAssert.AreEqual(expected, new[] { 3, 1, 5, 4, 2 }.BubbleSort());
-        }
-
-        [TestMethod]
-        public void BubbleSort_2()
-        {
-            var expected = Enumerable.Range(0, 10000).ToArray();
-            var target = expected.Reverse().ToArray();
-            CollectionAssert.AreEqual(expected, target.BubbleSort());
-        }
-
-        [TestMethod]
-        public void BubbleSort_3()
-        {
-            var target = Enumerable.Range(1, 20).Select(i => i * i).ToArray();
-            var actual = target.BubbleSort(i => i.ToString());
-
-            foreach (var i in actual)
-                Console.WriteLine(i);
-        }
     }
 }

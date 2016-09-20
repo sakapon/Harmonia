c776f2138395b7705ecadaa1387c7e3e60f1e684
Keiho Sakapon <git@saka-pon.net>
2016-09-20 18:16:39 +0900

    Add Quick Sort

---
+++ Harmonia/Harmonia/Sort/QuickSortHelper.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Harmonia.Sort
+{
+    public static class QuickSortHelper
+    {
+        public static void QuickSort<TSource>(this IList<TSource> source, Func<TSource, TSource, int> compare) =>
+            QuickSort(source, compare, 0, source.Count);
+
+        public static void QuickSort<TSource>(this IList<TSource> source, IComparer<TSource> comparer) =>
+            source.QuickSort((x1, x2) => comparer.Compare(x1, x2));
+
+        public static void QuickSort<TSource>(this IList<TSource> source) =>
+            source.QuickSort(Comparer<TSource>.Default);
+
+        public static void QuickSort2<TSource>(this IList<TSource> source) where TSource : IComparable<TSource> =>
+            source.QuickSort((x1, x2) => x1.CompareTo(x2));
+
+        public static void QuickSort<TSource>(this IList<TSource> source, Func<TSource, TSource, int> compare, int start, int count)
+        {
+            if (count < 2) return;
+
+            // 長さが 2 の場合は最適化します。
+            if (count == 2)
+            {
+                if (compare(source[start], source[start + 1]) > 0)
+                    source.Swap(start, start + 1);
+                return;
+            }
+
+            var m = source[start];
+            // cursor1: x <= m が保証されるインデックス。
+            // cursor2: x > m が保証されるインデックス。
+            var cursor1 = start;
+            var cursor2 = start + count;
+
+            while (true)
+            {
+                cursor1 = AssertLessThanOrEqual(source, compare, cursor1 + 1, cursor2 - 1, m);
+                if (cursor1 == cursor2 - 1)
+                    break;
+
+                cursor2 = AssertGreaterThan(source, compare, cursor2 - 1, cursor1 + 2, m);
+                if (cursor2 == cursor1 + 2)
+                {
+                    cursor2--;
+                    break;
+                }
+
+                cursor1++;
+                cursor2--;
+                source.Swap(cursor1, cursor2);
+            }
+
+            if (start != cursor1)
+                source.Swap(start, cursor1);
+
+            // start <= i < cursor1
+            source.QuickSort(compare, start, cursor1 - start);
+            // cursor2 <= i < start + count
+            source.QuickSort(compare, cursor2, start + count - cursor2);
+        }
+
+        static int AssertLessThanOrEqual<TSource>(this IList<TSource> source, Func<TSource, TSource, int> compare, int start, int end, TSource m)
+        {
+            for (var i = start; i <= end; i++)
+                if (compare(source[i], m) > 0)
+                    return i - 1;
+
+            return end;
+        }
+
+        static int AssertGreaterThan<TSource>(this IList<TSource> source, Func<TSource, TSource, int> compare, int start, int end, TSource m)
+        {
+            for (var i = start; i >= end; i--)
+                if (compare(source[i], m) <= 0)
+                    return i + 1;
+
+            return end;
+        }
+    }
+}
---
+++ Harmonia/UnitTest/Sort/QuickSortHelperTest.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Linq;
+using Harmonia.Sort;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTest.Sort
+{
+    [TestClass]
+    public class QuickSortHelperTest
+    {
+        [TestMethod]
+        public void QuickSort_1()
+        {
+            var start = 1;
+            var count = 10000;
+            var expected = Enumerable.Range(start, count).ToArray();
+
+            for (var i = 0; i < 10; i++)
+            {
+                var target = RandomHelper.ShuffleRange(start, count).ToArray();
+                target.QuickSort();
+                CollectionAssert.AreEqual(expected, target);
+            }
+        }
+
+        [TestMethod]
+        public void QuickSort_2()
+        {
+            var start = 1;
+            var count = 10000;
+            var expected = Enumerable.Range(start, count).ToArray();
+            var target = Enumerable.Range(start, count).ToArray();
+            target.QuickSort();
+            CollectionAssert.AreEqual(expected, target);
+        }
+
+        [TestMethod]
+        public void QuickSort_3()
+        {
+            var start = 1;
+            var count = 10000;
+            var expected = Enumerable.Range(start, count).ToArray();
+            var target = Enumerable.Range(start, count).Reverse().ToArray();
+            target.QuickSort();
+            CollectionAssert.AreEqual(expected, target);
+        }
+    }
+}

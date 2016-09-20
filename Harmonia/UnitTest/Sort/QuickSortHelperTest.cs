15ea73ffc23b5699e66685aa6495bd6af94eccc1
Keiho Sakapon <git@saka-pon.net>
2016-09-20 18:30:16 +0900

    Update test

--- Harmonia/UnitTest/Sort/QuickSortHelperTest.cs
+++ Harmonia/UnitTest/Sort/QuickSortHelperTest.cs
@@ -17,8 +17,15 @@ namespace UnitTest.Sort
 
             for (var i = 0; i < 10; i++)
             {
+                var comparisons = 0;
                 var target = RandomHelper.ShuffleRange(start, count).ToArray();
-                target.QuickSort();
+                target.QuickSort((x1, x2) =>
+                {
+                    comparisons++;
+                    return x1.CompareTo(x2);
+                });
+
+                Console.WriteLine(comparisons);
                 CollectionAssert.AreEqual(expected, target);
             }
         }
@@ -28,9 +35,18 @@ namespace UnitTest.Sort
         {
             var start = 1;
             var count = 10000;
+            var expectedComparisons = count * (count - 1) / 2;
             var expected = Enumerable.Range(start, count).ToArray();
+
+            var comparisons = 0;
             var target = Enumerable.Range(start, count).ToArray();
-            target.QuickSort();
+            target.QuickSort((x1, x2) =>
+            {
+                comparisons++;
+                return x1.CompareTo(x2);
+            });
+
+            Assert.AreEqual(expectedComparisons, comparisons);
             CollectionAssert.AreEqual(expected, target);
         }
 
@@ -39,9 +55,18 @@ namespace UnitTest.Sort
         {
             var start = 1;
             var count = 10000;
+            var expectedComparisons = count * (count - 1) / 2;
             var expected = Enumerable.Range(start, count).ToArray();
+
+            var comparisons = 0;
             var target = Enumerable.Range(start, count).Reverse().ToArray();
-            target.QuickSort();
+            target.QuickSort((x1, x2) =>
+            {
+                comparisons++;
+                return x1.CompareTo(x2);
+            });
+
+            Assert.AreEqual(expectedComparisons, comparisons);
             CollectionAssert.AreEqual(expected, target);
         }
     }

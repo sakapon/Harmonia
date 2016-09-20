c897f2bd82b23f1715c4aaa89467296e0832a56c
Keiho Sakapon <git@saka-pon.net>
2016-09-20 18:46:20 +0900

    Update test

--- Harmonia/UnitTest/Sort/BubbleSortHelperTest.cs
+++ Harmonia/UnitTest/Sort/BubbleSortHelperTest.cs
@@ -13,12 +13,20 @@ namespace UnitTest.Sort
         {
             var start = 1;
             var count = 1000;
+            var expectedComparisons = count * (count - 1) / 2;
             var expected = Enumerable.Range(start, count).ToArray();
 
             for (var i = 0; i < 10; i++)
             {
+                var comparisons = 0;
                 var target = RandomHelper.ShuffleRange(start, count).ToArray();
-                target.BubbleSort();
+                target.BubbleSort((x1, x2) =>
+                {
+                    comparisons++;
+                    return x1.CompareTo(x2);
+                });
+
+                Assert.AreEqual(expectedComparisons, comparisons);
                 CollectionAssert.AreEqual(expected, target);
             }
         }
@@ -28,9 +36,18 @@ namespace UnitTest.Sort
         {
             var start = 1;
             var count = 10000;
+            var expectedComparisons = count * (count - 1) / 2;
             var expected = Enumerable.Range(start, count).ToArray();
+
+            var comparisons = 0;
             var target = Enumerable.Range(start, count).ToArray();
-            target.BubbleSort();
+            target.BubbleSort((x1, x2) =>
+            {
+                comparisons++;
+                return x1.CompareTo(x2);
+            });
+
+            Assert.AreEqual(expectedComparisons, comparisons);
             CollectionAssert.AreEqual(expected, target);
         }
 
@@ -39,9 +56,18 @@ namespace UnitTest.Sort
         {
             var start = 1;
             var count = 10000;
+            var expectedComparisons = count * (count - 1) / 2;
             var expected = Enumerable.Range(start, count).ToArray();
+
+            var comparisons = 0;
             var target = Enumerable.Range(start, count).Reverse().ToArray();
-            target.BubbleSort();
+            target.BubbleSort((x1, x2) =>
+            {
+                comparisons++;
+                return x1.CompareTo(x2);
+            });
+
+            Assert.AreEqual(expectedComparisons, comparisons);
             CollectionAssert.AreEqual(expected, target);
         }
 

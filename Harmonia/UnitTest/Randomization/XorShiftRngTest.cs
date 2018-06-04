ffa6d47692d1d57635009772573f83761f676c02
Keiho Sakapon <git@saka-pon.net>
2018-06-04 18:28:13 +0900

    Add test

---
+++ Harmonia/UnitTest/Randomization/XorShiftRngTest.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using Harmonia.Randomization;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTest.Randomization
+{
+    [TestClass]
+    public class XorShiftRngTest
+    {
+        [TestMethod]
+        public void GenerateInt32()
+        {
+            var rng = new XorShiftRng();
+
+            for (var i = 0; i < 100; i++)
+                Console.WriteLine(rng.GenerateInt32());
+        }
+
+        [TestMethod]
+        public void GenerateInt32_Seed()
+        {
+            var rng = new XorShiftRng(1);
+
+            for (var i = 0; i < 100; i++)
+                Console.WriteLine(rng.GenerateInt32());
+        }
+
+        [TestMethod]
+        public void GenerateInt64()
+        {
+            var rng = new XorShiftRng();
+
+            for (var i = 0; i < 100; i++)
+                Console.WriteLine(rng.GenerateInt64());
+        }
+
+        [TestMethod]
+        public void GenerateInt64_Seed()
+        {
+            var rng = new XorShiftRng(1);
+
+            for (var i = 0; i < 100; i++)
+                Console.WriteLine(rng.GenerateInt64());
+        }
+
+        [TestMethod]
+        public void GenerateDouble()
+        {
+            var rng = new XorShiftRng();
+
+            for (var i = 0; i < 100; i++)
+            {
+                var x = rng.GenerateDouble();
+                Assert.IsTrue(0 <= x && x < 1);
+                Console.WriteLine(x);
+            }
+        }
+    }
+}

6e3021f72c9226d83543baa3799f23a7a9b02817
Keiho Sakapon <git@saka-pon.net>
2018-06-09 18:14:06 +0900

    Add methods

--- Harmonia/Harmonia/Conversion/UrlEncoding.cs
+++ Harmonia/Harmonia/Conversion/UrlEncoding.cs
@@ -3,14 +3,16 @@ using System.Collections.Generic;
 using System.Globalization;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 
 namespace Harmonia.Conversion
 {
     public static class UrlEncoding
     {
-        const string Rfc3986_UnreservedChars = "-._~"; // 4 symbols
-        const string Rfc3986_ReservedChars = "!#$&'()*+,/:;=?@[]"; // 18 symbols
-        const string Rfc3986_OtherChars = " \"%<>\\^`{|}"; // 11 symbols
+        internal const string Alphanumerics = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+        internal const string Rfc3986_UnreservedChars = "-._~"; // 4 symbols
+        internal const string Rfc3986_ReservedChars = "!#$&'()*+,/:;=?@[]"; // 18 symbols
+        internal const string Rfc3986_OtherChars = " \"%<>\\^`{|}"; // 11 symbols
 
         public static string PercentEncode(this string value)
         {
@@ -27,6 +29,13 @@ namespace Harmonia.Conversion
                 .Select(s => byte.Parse(s, NumberStyles.HexNumber))
                 .ToArray();
             return Encoding.UTF8.GetString(bytes);
+        }
+
+        public static string UrlEncode(string value)
+        {
+            if (value == null) throw new ArgumentNullException(nameof(value));
+
+            return Regex.Replace(value, "[^-._~0-9A-Za-z]+", m => PercentEncode(m.Value));
         }
     }
 }
--- Harmonia/UnitTest/Conversion/UrlEncodingTest.cs
+++ Harmonia/UnitTest/Conversion/UrlEncodingTest.cs
@@ -10,11 +10,21 @@ namespace UnitTest.Conversion
         [TestMethod]
         public void PercentEncode()
         {
-            var original = " !\"-あ";
-            var encoded = "%20%21%22%2D%E3%81%82";
+            var original = " !\"-0Aaあ";
+            var encoded = "%20%21%22%2D%30%41%61%E3%81%82";
 
             Assert.AreEqual(encoded, UrlEncoding.PercentEncode(original));
             Assert.AreEqual(original, UrlEncoding.PercentDecode(encoded));
+        }
+
+        [TestMethod]
+        public void UrlEncode()
+        {
+            Assert.AreEqual(UrlEncoding.Alphanumerics, UrlEncoding.UrlEncode(UrlEncoding.Alphanumerics));
+            Assert.AreEqual(UrlEncoding.Rfc3986_UnreservedChars, UrlEncoding.UrlEncode(UrlEncoding.Rfc3986_UnreservedChars));
+            Assert.AreEqual(UrlEncoding.PercentEncode(UrlEncoding.Rfc3986_ReservedChars), UrlEncoding.UrlEncode(UrlEncoding.Rfc3986_ReservedChars));
+            Assert.AreEqual(UrlEncoding.PercentEncode(UrlEncoding.Rfc3986_OtherChars), UrlEncoding.UrlEncode(UrlEncoding.Rfc3986_OtherChars));
+            Assert.AreEqual("%20%21%22-0Aa%E3%81%82", UrlEncoding.UrlEncode(" !\"-0Aaあ"));
         }
     }
 }

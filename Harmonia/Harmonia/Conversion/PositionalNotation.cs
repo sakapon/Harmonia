c4812a19c60fcb86cc6d769590860887a3c63c4d
Keiho Sakapon <git@saka-pon.net>
2018-06-10 01:49:22 +0900

    Add class for positional notation

---
+++ Harmonia/Harmonia/Conversion/PositionalNotation.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Harmonia.Conversion
+{
+    public static class PositionalNotation
+    {
+        const string NumberChars = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        static readonly Dictionary<char, int> CharIndexMap = NumberChars.Select((c, i) => new { c, i }).ToDictionary(_ => _.c, _ => _.i);
+
+        public static string ToStringInBase(this int value, int toBase)
+        {
+            if (value == 0) return "0";
+            if (value < 0) return "-" + ToStringInBase(-value, toBase);
+
+            var result = "";
+            var current = value;
+            while (current > 0)
+            {
+                result = NumberChars[current % toBase].ToString() + result;
+                current /= toBase;
+            }
+            return result;
+        }
+
+        public static int FromStringInBase(this string value, int fromBase)
+        {
+            if (value == null) throw new ArgumentNullException(nameof(value));
+            if (value.StartsWith("-")) return -FromStringInBase(value.Substring(1), fromBase);
+
+            var result = 0;
+            for (var i = 0; i < value.Length; i++)
+            {
+                result *= fromBase;
+                result += CharIndexMap[value[i]];
+            }
+            return result;
+        }
+    }
+}
---
+++ Harmonia/UnitTest/Conversion/PositionalNotationTest.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Harmonia.Conversion;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTest.Conversion
+{
+    [TestClass]
+    public class PositionalNotationTest
+    {
+        [TestMethod]
+        public void ToStringInBase()
+        {
+            void Test(int @base, int n, string s)
+            {
+                Assert.AreEqual(s, n.ToStringInBase(@base));
+                Assert.AreEqual(n, s.FromStringInBase(@base));
+            }
+
+            Test(2, 0, "0");
+            Test(16, -15, "-F");
+            Test(10, 123, "123");
+
+            Test(2, 23, "10111");
+            Test(3, 729, "1000000");
+            Test(8, 123, "173");
+            Test(8, 83, "123");
+            Test(12, 123, "A3");
+            Test(12, 171, "123");
+            Test(16, 65535, "FFFF");
+            Test(20, 99, "4J");
+        }
+    }
+}

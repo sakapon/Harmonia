21699d1e01be244ea107148eba9454a3ac3e35cb
Keiho Sakapon <git@saka-pon.net>
2019-01-24 16:07:35 +0900

    Add methods

--- Harmonia/Harmonia/Numerics/ElementaryFunctions.cs
+++ Harmonia/Harmonia/Numerics/ElementaryFunctions.cs
@@ -35,5 +35,61 @@ namespace Harmonia.Numerics
             }
             return sum;
         }
+
+        public static double Sin(double x)
+        {
+            var _x2 = -x * x;
+            var c = 1.0;
+            var p = x;
+            var sum = x;
+
+            for (var i = 3; i < 100; i += 2)
+            {
+                c *= i * (i - 1);
+                p *= _x2;
+
+                var temp = sum + p / c;
+                if (sum == temp) return sum;
+                sum = temp;
+            }
+            return sum;
+        }
+
+        public static double Cos(double x)
+        {
+            var _x2 = -x * x;
+            var c = 1.0;
+            var p = 1.0;
+            var sum = 1.0;
+
+            for (var i = 2; i < 100; i += 2)
+            {
+                c *= i * (i - 1);
+                p *= _x2;
+
+                var temp = sum + p / c;
+                if (sum == temp) return sum;
+                sum = temp;
+            }
+            return sum;
+        }
+
+        public static double Exp(double x)
+        {
+            var c = 1.0;
+            var p = 1.0;
+            var sum = 1.0;
+
+            for (var i = 1; i < 100; i++)
+            {
+                c *= i;
+                p *= x;
+
+                var temp = sum + p / c;
+                if (sum == temp) break;
+                sum = temp;
+            }
+            return sum;
+        }
     }
 }
--- Harmonia/UnitTest/Numerics/ElementaryFunctionsTest.cs
+++ Harmonia/UnitTest/Numerics/ElementaryFunctionsTest.cs
@@ -15,5 +15,53 @@ namespace UnitTest.Numerics
 
         [TestMethod]
         public void GetE() => AssertNearlyEqual(Math.E, ElementaryFunctions.GetE());
+
+        [TestMethod]
+        public void Sin()
+        {
+            void Test(double expected, double x) => AssertNearlyEqual(expected, ElementaryFunctions.Sin(x));
+
+            Test(1.0, Math.PI / 2);
+            Test(Math.Sqrt(3) / 2, Math.PI / 3);
+            Test(Math.Sqrt(2) / 2, Math.PI / 4);
+            Test(0.5, Math.PI / 6);
+            Test(0.0, 0);
+            Test(-0.5, -Math.PI / 6);
+            Test(-Math.Sqrt(2) / 2, -Math.PI / 4);
+            Test(-Math.Sqrt(3) / 2, -Math.PI / 3);
+            Test(-1.0, -Math.PI / 2);
+
+            Test((Math.Sqrt(6) - Math.Sqrt(2)) / 4, Math.PI / 12);
+        }
+
+        [TestMethod]
+        public void Cos()
+        {
+            void Test(double expected, double x) => AssertNearlyEqual(expected, ElementaryFunctions.Cos(x));
+
+            Test(1.0, 0);
+            Test(Math.Sqrt(3) / 2, Math.PI / 6);
+            Test(Math.Sqrt(2) / 2, Math.PI / 4);
+            Test(0.5, Math.PI / 3);
+            Test(0.0, Math.PI / 2);
+            Test(-0.5, Math.PI * 2 / 3);
+            Test(-Math.Sqrt(2) / 2, Math.PI * 3 / 4);
+            Test(-Math.Sqrt(3) / 2, Math.PI * 5 / 6);
+            Test(-1.0, Math.PI);
+
+            Test((Math.Sqrt(6) + Math.Sqrt(2)) / 4, Math.PI / 12);
+        }
+
+        [TestMethod]
+        public void Exp()
+        {
+            void Test(double x) => AssertNearlyEqual(Math.Exp(x), ElementaryFunctions.Exp(x));
+
+            Test(1.0);
+            Test(-1.0);
+            Test(0.5);
+            Test(2.0);
+            Test(Math.PI);
+        }
     }
 }

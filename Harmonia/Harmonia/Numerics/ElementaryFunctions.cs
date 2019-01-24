a395d8a56422cf48192fff5093b6ea68ae7077e3
Keiho Sakapon <git@saka-pon.net>
2019-01-24 15:07:18 +0900

    Add classes

---
+++ Harmonia/Harmonia/Numerics/ElementaryFunctions.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Harmonia.Numerics
+{
+    public static class ElementaryFunctions
+    {
+        public static double GetPi()
+        {
+            var p = 1.0;
+            var sum = 1.0;
+
+            for (var i = 3; i < 100; i += 2)
+            {
+                p *= -3;
+
+                var temp = sum + 1 / (i * p);
+                if (sum == temp) break;
+                sum = temp;
+            }
+            return Math.Sqrt(12) * sum;
+        }
+
+        public static double GetE()
+        {
+            var c = 1.0;
+            var sum = 1.0;
+
+            for (var i = 1; i < 100; i++)
+            {
+                c *= i;
+
+                var temp = sum + 1 / c;
+                if (sum == temp) break;
+                sum = temp;
+            }
+            return sum;
+        }
+    }
+}
---
+++ Harmonia/UnitTest/Numerics/ElementaryFunctionsTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Harmonia.Numerics;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTest.Numerics
+{
+    [TestClass]
+    public class ElementaryFunctionsTest
+    {
+        static void AssertNearlyEqual(double expected, double actual) =>
+            Assert.AreEqual(0.0, Math.Round(expected - actual, 12));
+
+        [TestMethod]
+        public void GetPi() => AssertNearlyEqual(Math.PI, ElementaryFunctions.GetPi());
+
+        [TestMethod]
+        public void GetE() => AssertNearlyEqual(Math.E, ElementaryFunctions.GetE());
+    }
+}

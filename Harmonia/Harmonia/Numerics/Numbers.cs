8d90573c7c831304b7a3a763ee285f71fee5c863
Keiho Sakapon <git@saka-pon.net>
2019-01-24 18:39:46 +0900

    Add classes

---
+++ Harmonia/Harmonia/Numerics/Numbers.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Harmonia.Numerics
+{
+    public static class Numbers
+    {
+        public static int Gcd(int x, int y)
+        {
+            if (x <= 0) throw new ArgumentOutOfRangeException(nameof(x), x, "The value must be a natural number.");
+            if (y <= 0) throw new ArgumentOutOfRangeException(nameof(y), y, "The value must be a natural number.");
+
+            var M = x;
+            var m = y;
+
+            while (true)
+            {
+                var r = M % m;
+                if (r == 0) return m;
+                M = m;
+                m = r;
+            }
+        }
+
+        public static int Lcm(int x, int y) => x * y / Gcd(x, y);
+
+        public static bool IsSquareNumber(int x)
+        {
+            var r = Math.Sqrt(x);
+            return Math.Round(r) == r;
+        }
+    }
+}
---
+++ Harmonia/UnitTest/Numerics/NumbersTest.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Harmonia.Numerics;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTest.Numerics
+{
+    [TestClass]
+    public class NumbersTest
+    {
+        [TestMethod]
+        public void Gcd()
+        {
+            Assert.AreEqual(1, Numbers.Gcd(1, 1));
+            Assert.AreEqual(2, Numbers.Gcd(2, 2));
+            Assert.AreEqual(6, Numbers.Gcd(6, 6));
+            Assert.AreEqual(1, Numbers.Gcd(2, 3));
+            Assert.AreEqual(2, Numbers.Gcd(2, 4));
+            Assert.AreEqual(2, Numbers.Gcd(4, 6));
+            Assert.AreEqual(2, Numbers.Gcd(8, 10));
+            Assert.AreEqual(3, Numbers.Gcd(15, 21));
+        }
+
+        [TestMethod]
+        public void Lcm()
+        {
+            Assert.AreEqual(1, Numbers.Lcm(1, 1));
+            Assert.AreEqual(2, Numbers.Lcm(2, 2));
+            Assert.AreEqual(6, Numbers.Lcm(6, 6));
+            Assert.AreEqual(6, Numbers.Lcm(2, 3));
+            Assert.AreEqual(4, Numbers.Lcm(2, 4));
+            Assert.AreEqual(12, Numbers.Lcm(4, 6));
+            Assert.AreEqual(40, Numbers.Lcm(8, 10));
+            Assert.AreEqual(105, Numbers.Lcm(15, 21));
+        }
+
+        [TestMethod]
+        public void IsSquareNumber()
+        {
+            Assert.AreEqual(true, Numbers.IsSquareNumber(1));
+            Assert.AreEqual(false, Numbers.IsSquareNumber(2));
+            Assert.AreEqual(false, Numbers.IsSquareNumber(3));
+            Assert.AreEqual(true, Numbers.IsSquareNumber(4));
+            Assert.AreEqual(false, Numbers.IsSquareNumber(5));
+        }
+    }
+}

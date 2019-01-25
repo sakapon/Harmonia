4c9f03aeb36cb039af64e444006d2ee12d40d194
Keiho Sakapon <git@saka-pon.net>
2019-01-25 14:59:13 +0900

    Add classes

---
+++ Harmonia/Harmonia/Numerics/ElementaryFunctions2.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Harmonia.Numerics
+{
+    public static class ElementaryFunctions2
+    {
+        public static double SumForApproximation(this IEnumerable<double> terms)
+        {
+            var sum = 0.0;
+
+            foreach (var v in terms)
+            {
+                var temp = sum + v;
+                if (sum == temp) break;
+                sum = temp;
+            }
+            return sum;
+        }
+
+        public static double Exp(double x)
+        {
+            return GetTerms().SumForApproximation();
+
+            IEnumerable<double> GetTerms()
+            {
+                var c = 1.0;
+                var p = 1.0;
+                yield return 1.0;
+
+                for (var i = 1; i < 100; i++)
+                {
+                    c *= i;
+                    p *= x;
+                    yield return p / c;
+                }
+            }
+        }
+
+        public static double Log(double x)
+        {
+            if (x <= 0) throw new ArgumentOutOfRangeException(nameof(x), x, "The value must be positive.");
+
+            return 2 * GetTerms().SumForApproximation();
+
+            IEnumerable<double> GetTerms()
+            {
+                // http://math-lab.main.jp/taylor7b.html
+                var t = (x - 1) / (x + 1);
+
+                var t2 = t * t;
+                var p = t;
+                yield return t;
+
+                for (var i = 3; i < 100; i += 2)
+                {
+                    p *= t2;
+                    yield return p / i;
+                }
+            }
+        }
+    }
+}
---
+++ Harmonia/UnitTest/Numerics/ElementaryFunctions2Test.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Harmonia.Numerics;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTest.Numerics
+{
+    [TestClass]
+    public class ElementaryFunctions2Test
+    {
+        static void AssertNearlyEqual(double expected, double actual) =>
+            Assert.AreEqual(0.0, Math.Round(expected - actual, 12));
+
+        [TestMethod]
+        public void Exp()
+        {
+            void Test(double x) => AssertNearlyEqual(Math.Exp(x), ElementaryFunctions2.Exp(x));
+
+            Test(1.0);
+            Test(-1.0);
+            Test(0.5);
+            Test(2.0);
+            Test(Math.PI);
+        }
+
+        [TestMethod]
+        public void Log()
+        {
+            void Test(double x) => AssertNearlyEqual(Math.Log(x), ElementaryFunctions2.Log(x));
+
+            Test(1.0);
+            Test(2.0);
+            Test(3.0);
+            Test(0.5);
+            Test(Math.E);
+        }
+    }
+}

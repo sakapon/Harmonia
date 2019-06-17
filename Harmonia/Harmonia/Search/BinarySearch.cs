ccb949e3aa1379a5d8a80e6e95e3e3eacad2a873
Keiho Sakapon <git@saka-pon.net>
2019-06-17 16:45:36 +0900

    Add class for binary search

---
+++ Harmonia/Harmonia/Search/BinarySearch.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Harmonia.Search
+{
+    public static class BinarySearch
+    {
+        public static int GetIndex(int[] array, int value)
+        {
+            if (array == null) throw new ArgumentNullException(nameof(array));
+            if (array.Length == 0) return -1;
+
+            return Search(0, array.Length);
+
+            int Search(int start, int count)
+            {
+                if (count == 1)
+                    return array[start] == value ? start : -1;
+
+                var m = start + (count >> 1);
+                return array[m] > value ?
+                    Search(start, m - start) :
+                    Search(m, start + count - m);
+            }
+        }
+    }
+}
---
+++ Harmonia/UnitTest/Search/BinarySearchTest.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTest.Search
+{
+    [TestClass]
+    public class BinarySearchTest
+    {
+    }
+}

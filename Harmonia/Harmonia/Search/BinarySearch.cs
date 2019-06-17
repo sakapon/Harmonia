83a56997beba87222493b8ede98cdf03c63aef0d
Keiho Sakapon <git@saka-pon.net>
2019-06-17 19:14:20 +0900

    Add method

--- Harmonia/Harmonia/Search/BinarySearch.cs
+++ Harmonia/Harmonia/Search/BinarySearch.cs
@@ -4,6 +4,7 @@ namespace Harmonia.Search
 {
     public static class BinarySearch
     {
+        // ソート済であることを前提とします。
         public static int GetIndex(int[] array, int value)
         {
             if (array == null) throw new ArgumentNullException(nameof(array));
@@ -18,7 +19,29 @@ namespace Harmonia.Search
 
                 var c1 = count >> 1;
                 var s2 = start + c1;
-                return array[s2] > value ?
+                return value < array[s2] ?
+                    Search(start, c1) :
+                    Search(s2, count - c1);
+            }
+        }
+
+        // ソート済であることを前提とします。
+        // a[i] <= x < a[i+1] のとき、i を返します。
+        public static int GetIndexByRange(int[] array, int value)
+        {
+            if (array == null) throw new ArgumentNullException(nameof(array));
+            if (array.Length == 0) return -1;
+
+            return Search(0, array.Length);
+
+            int Search(int start, int count)
+            {
+                if (count == 1)
+                    return array[start] <= value ? start : -1;
+
+                var c1 = count >> 1;
+                var s2 = start + c1;
+                return value < array[s2] ?
                     Search(start, c1) :
                     Search(s2, count - c1);
             }
--- Harmonia/UnitTest/Search/BinarySearchTest.cs
+++ Harmonia/UnitTest/Search/BinarySearchTest.cs
@@ -17,10 +17,29 @@ namespace UnitTest.Search
                 .ToArray();
 
             Assert.AreEqual(-1, BinarySearch.GetIndex(array, 2));
+            Assert.AreEqual(-1, BinarySearch.GetIndex(array, 2003));
+
             for (var i = 0; i < array.Length; i++)
             {
                 Assert.AreEqual(i, BinarySearch.GetIndex(array, array[i]));
                 Assert.AreEqual(-1, BinarySearch.GetIndex(array, array[i] + 1));
+            }
+        }
+
+        [TestMethod]
+        public void GetIndexByRange_Int32()
+        {
+            var array = Enumerable.Range(0, 1000)
+                .Select(i => 2 * i + 3)
+                .ToArray();
+
+            Assert.AreEqual(-1, BinarySearch.GetIndexByRange(array, 2));
+            Assert.AreEqual(999, BinarySearch.GetIndexByRange(array, 2003));
+
+            for (var i = 0; i < array.Length; i++)
+            {
+                Assert.AreEqual(i, BinarySearch.GetIndexByRange(array, array[i]));
+                Assert.AreEqual(i, BinarySearch.GetIndexByRange(array, array[i] + 1));
             }
         }
     }

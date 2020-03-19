e7b4644435b4487b3891cce8dcbfb862c29a7551
Keiho Sakapon <git@saka-pon.net>
2020-03-19 16:30:56 +0900

    Add comments

--- Harmonia/Harmonia/Search/BinarySearch.cs
+++ Harmonia/Harmonia/Search/BinarySearch.cs
@@ -2,11 +2,22 @@
 
 namespace Harmonia.Search
 {
+	/// <summary>
+	/// 二分探索のためのメソッドを提供します。
+	/// </summary>
 	public static class BinarySearch
 	{
-		// 条件 f を満たす最初の値を探索します。
-		// f(r) は評価されません。
-		// f(r - 1) が false のとき、r を返します。
+		/// <summary>
+		/// 条件 f を満たす最初の値を探索します。
+		/// [l, x) 上で false、[x, r) 上で true となる x を返します。
+		/// f(l) が true のとき、l を返します。
+		/// f(r - 1) が false のとき、r を返します。
+		/// f(r) は評価されません。
+		/// </summary>
+		/// <param name="l">探索範囲の下限。</param>
+		/// <param name="r">探索範囲の上限。</param>
+		/// <param name="f">半開区間 [l, r) 上で定義される条件。</param>
+		/// <returns>条件 f を満たす最初の値。</returns>
 		public static int First(int l, int r, Func<int, bool> f)
 		{
 			int m;
@@ -16,9 +27,17 @@ namespace Harmonia.Search
 			return r;
 		}
 
-		// 条件 f を満たす最後の値を探索します。
-		// f(l) は評価されません。
-		// f(l + 1) が false のとき、l を返します。
+		/// <summary>
+		/// 条件 f を満たす最後の値を探索します。
+		/// (l, x] 上で true、(x, r] 上で false となる x を返します。
+		/// f(r) が true のとき、r を返します。
+		/// f(l + 1) が false のとき、l を返します。
+		/// f(l) は評価されません。
+		/// </summary>
+		/// <param name="l">探索範囲の下限。</param>
+		/// <param name="r">探索範囲の上限。</param>
+		/// <param name="f">半開区間 (l, r] 上で定義される条件。</param>
+		/// <returns>条件 f を満たす最後の値。</returns>
 		public static int Last(int l, int r, Func<int, bool> f)
 		{
 			int m;
@@ -28,6 +47,16 @@ namespace Harmonia.Search
 			return l;
 		}
 
+		/// <summary>
+		/// 条件 f を満たす最初の値を指定された誤差の範囲内で探索します。
+		/// (l, x) 上で false、[x, r) 上で true となる x を返します。
+		/// r 近傍で false のとき、r を返します。
+		/// </summary>
+		/// <param name="l">探索範囲の下限。</param>
+		/// <param name="r">探索範囲の上限。</param>
+		/// <param name="f">開区間 (l, r) 上で定義される条件。</param>
+		/// <param name="digits">誤差を表す小数部の桁数。</param>
+		/// <returns>条件 f を満たす最初の値。</returns>
 		public static double First(double l, double r, Func<double, bool> f, int digits = 9)
 		{
 			double m;
@@ -37,6 +66,16 @@ namespace Harmonia.Search
 			return r;
 		}
 
+		/// <summary>
+		/// 条件 f を満たす最後の値を指定された誤差の範囲内で探索します。
+		/// (l, x] 上で true、(x, r) 上で false となる x を返します。
+		/// l 近傍で false のとき、l を返します。
+		/// </summary>
+		/// <param name="l">探索範囲の下限。</param>
+		/// <param name="r">探索範囲の上限。</param>
+		/// <param name="f">開区間 (l, r) 上で定義される条件。</param>
+		/// <param name="digits">誤差を表す小数部の桁数。</param>
+		/// <returns>条件 f を満たす最後の値。</returns>
 		public static double Last(double l, double r, Func<double, bool> f, int digits = 9)
 		{
 			double m;

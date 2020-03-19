3b6cd92fa16569b429d0078f2730703a8c3cc3af
Keiho Sakapon <git@saka-pon.net>
2020-03-19 13:03:36 +0900

    Add new class for binary search

---
+++ Harmonia/Harmonia/Search/BinarySearch.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace Harmonia.Search
+{
+	public static class BinarySearch
+	{
+		// 条件 f を満たす最初の値を探索します。
+		// f(r) は評価されません。
+		// f(r - 1) が false のとき、r を返します。
+		public static int First(int l, int r, Func<int, bool> f)
+		{
+			int m;
+			while (l < r)
+				if (f(m = l + (r - l - 1) / 2)) r = m;
+				else l = m + 1;
+			return r;
+		}
+
+		// 条件 f を満たす最後の値を探索します。
+		// f(l) は評価されません。
+		// f(l + 1) が false のとき、l を返します。
+		public static int Last(int l, int r, Func<int, bool> f)
+		{
+			int m;
+			while (l < r)
+				if (f(m = r - (r - l - 1) / 2)) l = m;
+				else r = m - 1;
+			return l;
+		}
+
+		public static double First(double l, double r, Func<double, bool> f, int digits = 9)
+		{
+			double m;
+			while (Math.Round(r - l, digits) > 0)
+				if (f(m = l + (r - l) / 2)) r = m;
+				else l = m;
+			return r;
+		}
+
+		public static double Last(double l, double r, Func<double, bool> f, int digits = 9)
+		{
+			double m;
+			while (Math.Round(r - l, digits) > 0)
+				if (f(m = r - (r - l) / 2)) l = m;
+				else r = m;
+			return l;
+		}
+	}
+}
---
+++ Harmonia/UnitTest/Search/BinarySearchTest.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Harmonia.Search;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTest.Search
+{
+	[TestClass]
+	public class BinarySearchTest
+	{
+		static readonly Random random = new Random();
+
+		[TestMethod]
+		public void FirstGt_Random()
+		{
+			for (int k = 0; k < 10; k++)
+			{
+				for (int n = 0; n < 10; n++) Test(n);
+				for (int n = 1000; n < 1010; n++) Test(n);
+			}
+
+			void Test(int n)
+			{
+				var a = Enumerable.Range(0, n).Select(_ => random.Next(0, n)).OrderBy(x => x).ToArray();
+				for (int x = -2; x < n + 2; x++)
+				{
+					var actual = BinarySearch.First(0, n, i => a[i] > x);
+					Assert.IsTrue(actual == n || a[actual] > x);
+					Assert.IsTrue(actual == 0 || a[actual - 1] <= x);
+				}
+			}
+		}
+
+		[TestMethod]
+		public void LastGt_Random()
+		{
+			for (int k = 0; k < 10; k++)
+			{
+				for (int n = 0; n < 10; n++) Test(n);
+				for (int n = 1000; n < 1010; n++) Test(n);
+			}
+
+			void Test(int n)
+			{
+				var a = Enumerable.Range(0, n).Select(_ => random.Next(0, n)).OrderBy(x => -x).ToArray();
+				for (int x = -2; x < n + 2; x++)
+				{
+					var actual = BinarySearch.Last(-1, n - 1, i => a[i] > x);
+					Assert.IsTrue(actual == -1 || a[actual] > x);
+					Assert.IsTrue(actual == n - 1 || a[actual + 1] <= x);
+				}
+			}
+		}
+	}
+}

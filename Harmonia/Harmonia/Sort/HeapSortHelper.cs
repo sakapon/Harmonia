b0afc9634aaa61c242db0d0cc436292416bdd2cf
Keiho Sakapon <git@saka-pon.net>
2020-03-19 23:50:53 +0900

    Add class for heap sort

---
+++ Harmonia/Harmonia/Sort/HeapSortHelper.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Harmonia.Trees;
+
+namespace Harmonia.Sort
+{
+	public static class HeapSortHelper
+	{
+		public static IEnumerable<TSource> HeapSort<TSource>(this IEnumerable<TSource> source, bool descending = false)
+		{
+			if (source == null) throw new ArgumentNullException(nameof(source));
+
+			var heap = Heap1.Create(source, descending);
+			while (heap.Any) yield return heap.Pop();
+		}
+
+		public static IEnumerable<TSource> HeapSort<TSource, TKey>(this IEnumerable<TSource> source, Func<TSource, TKey> keySelector, bool descending = false)
+		{
+			if (source == null) throw new ArgumentNullException(nameof(source));
+			if (keySelector == null) throw new ArgumentNullException(nameof(keySelector));
+
+			var heap = Heap1.Create(v => v.k, source.Select(o => (o: o, k: keySelector(o))), descending);
+			while (heap.Any) yield return heap.Pop().o;
+		}
+	}
+}
---
+++ Harmonia/UnitTest/Sort/HeapSortHelperTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+using Harmonia.Sort;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTest.Sort
+{
+	[TestClass]
+	public class HeapSortHelperTest
+	{
+		Random random = new Random();
+
+		[TestMethod]
+		public void SortTake()
+		{
+			var values = Enumerable.Range(0, 300000).Select(i => random.Next(300000)).ToArray();
+			var actual = TestHelper.MeasureTime(() => values.HeapSort().Take(1000).ToArray());
+			var expected = TestHelper.MeasureTime(() => values.OrderBy(x => x).Take(1000).ToArray());
+			CollectionAssert.AreEqual(expected, actual);
+		}
+	}
+}

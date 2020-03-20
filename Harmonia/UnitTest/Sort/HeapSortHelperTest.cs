7f77075d988b08c80f5cf13c000ea9fdaaf8d4a4
Keiho Sakapon <git@saka-pon.net>
2020-03-20 21:51:00 +0900

    Add test

--- Harmonia/UnitTest/Sort/HeapSortHelperTest.cs
+++ Harmonia/UnitTest/Sort/HeapSortHelperTest.cs
@@ -10,12 +10,41 @@ namespace UnitTest.Sort
 	{
 		Random random = new Random();
 
+		int[] CreateData() => Enumerable.Range(0, 300000).Select(i => random.Next(300000)).ToArray();
+
 		[TestMethod]
-		public void SortTake()
+		public void SortTake_Asc()
 		{
-			var values = Enumerable.Range(0, 300000).Select(i => random.Next(300000)).ToArray();
+			var values = CreateData();
 			var actual = TestHelper.MeasureTime(() => values.HeapSort().Take(1000).ToArray());
 			var expected = TestHelper.MeasureTime(() => values.OrderBy(x => x).Take(1000).ToArray());
+			CollectionAssert.AreEqual(expected, actual);
+		}
+
+		[TestMethod]
+		public void SortTake_Desc()
+		{
+			var values = CreateData();
+			var actual = TestHelper.MeasureTime(() => values.HeapSort(true).Take(1000).ToArray());
+			var expected = TestHelper.MeasureTime(() => values.OrderBy(x => -x).Take(1000).ToArray());
+			CollectionAssert.AreEqual(expected, actual);
+		}
+
+		[TestMethod]
+		public void SortTake_Key_Asc()
+		{
+			var values = CreateData();
+			var actual = TestHelper.MeasureTime(() => values.HeapSort(x => x.ToString()).Take(1000).ToArray());
+			var expected = TestHelper.MeasureTime(() => values.OrderBy(x => x.ToString()).Take(1000).ToArray());
+			CollectionAssert.AreEqual(expected, actual);
+		}
+
+		[TestMethod]
+		public void SortTake_Key_Desc()
+		{
+			var values = CreateData();
+			var actual = TestHelper.MeasureTime(() => values.HeapSort(x => x.ToString(), true).Take(1000).ToArray());
+			var expected = TestHelper.MeasureTime(() => values.OrderByDescending(x => x.ToString()).Take(1000).ToArray());
 			CollectionAssert.AreEqual(expected, actual);
 		}
 	}

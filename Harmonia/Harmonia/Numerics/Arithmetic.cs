c4e81569f60ef26dd0157feb6a7638e8b2be602f
Keiho Sakapon <git@saka-pon.net>
2020-05-11 09:45:13 +0900

    Add class for arithmetic

---
+++ Harmonia/Harmonia/Numerics/Arithmetic.cs
@@ -0,0 +1,35 @@
+﻿namespace Harmonia.Numerics
+{
+	public static class Arithmetic
+	{
+		public static int Add(int x, int y)
+		{
+			var r = x ^ y;
+			var t = (x & y) << 1;
+			return t == 0 ? r : Add(r, t);
+		}
+
+		public static int Subtract(int x, int y)
+		{
+			return Add(x, Add(~y, 1));
+		}
+
+		// unsigned
+		public static byte Multiply(byte x, byte y)
+		{
+			byte r = 0;
+			for (; x != 0 && y != 0; x <<= 1, y >>= 1)
+				if ((y & 1) != 0) r += x;
+			return r;
+		}
+
+		// signed
+		public static int Multiply(int x, int y)
+		{
+			var r = 0;
+			for (; x != 0 && y != 0; x <<= 1, y >>= 1)
+				if ((y & 1) != 0) r += x;
+			return r;
+		}
+	}
+}
---
+++ Harmonia/UnitTest/Numerics/ArithmeticTest.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using Harmonia.Numerics;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTest.Numerics
+{
+	[TestClass]
+	public class ArithmeticTest
+	{
+		[TestMethod]
+		public void Add_Int32()
+		{
+			void Test(int x, int y) => Assert.AreEqual(x + y, Arithmetic.Add(x, y));
+
+			Test(6, 9);
+			Test(15, 17);
+			Test(-3, 5);
+			Test(3, -5);
+			Test(-3, -5);
+			Test(0xFFFF, 1);
+			Test(8, int.MaxValue);
+		}
+
+		[TestMethod]
+		public void Subtract_Int32()
+		{
+			void Test(int x, int y) => Assert.AreEqual(x - y, Arithmetic.Subtract(x, y));
+
+			Test(6, 9);
+			Test(5, 3);
+			Test(-3, 5);
+			Test(3, -5);
+			Test(-3, -5);
+			Test(0xFFFF, -1);
+			Test(8, int.MaxValue);
+		}
+
+		[TestMethod]
+		public void Multiply_Byte()
+		{
+			void Test(byte x, byte y) => Assert.AreEqual((byte)(x * y), Arithmetic.Multiply(x, y));
+
+			Test(6, 5);
+			Test(15, 17);
+			Test(8, 32);
+			Test(51, 53);
+		}
+
+		[TestMethod]
+		public void Multiply_Int32()
+		{
+			void Test(int x, int y) => Assert.AreEqual(x * y, Arithmetic.Multiply(x, y));
+
+			Test(6, 5);
+			Test(15, 17);
+			Test(-3, 5);
+			Test(3, -5);
+			Test(-3, -5);
+			Test(8, int.MaxValue);
+			Test(0x10000000, -4);
+		}
+	}
+}

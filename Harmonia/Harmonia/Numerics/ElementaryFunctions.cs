e006cf8f6fb1ad02f9f29222bd48cfac13d9eae5
Keiho Sakapon <git@saka-pon.net>
2020-05-11 09:57:31 +0900

    Move functions

--- Harmonia/Harmonia/Numerics/ElementaryFunctions.cs
+++ Harmonia/Harmonia/Numerics/ElementaryFunctions.cs
@@ -169,35 +169,5 @@ namespace Harmonia.Numerics
                 if (r == (temp = r * (2 - x * r))) break;
             return r;
         }
-
-        public static int Add(int x, int y)
-        {
-            var r = x ^ y;
-            var t = (x & y) << 1;
-            return t == 0 ? r : Add(r, t);
-        }
-
-        public static int Subtract(int x, int y)
-        {
-            return Add(x, Add(~y, 1));
-        }
-
-        // unsigned
-        public static byte Multiply(byte x, byte y)
-        {
-            byte r = 0;
-            for (; x != 0 && y != 0; x <<= 1, y >>= 1)
-                if ((y & 1) != 0) r += x;
-            return r;
-        }
-
-        // signed
-        public static int Multiply(int x, int y)
-        {
-            var r = 0;
-            for (; x != 0 && y != 0; x <<= 1, y >>= 1)
-                if ((y & 1) != 0) r += x;
-            return r;
-        }
     }
 }
--- Harmonia/UnitTest/Numerics/ElementaryFunctionsTest.cs
+++ Harmonia/UnitTest/Numerics/ElementaryFunctionsTest.cs
@@ -159,58 +159,5 @@ namespace UnitTest.Numerics
             Test(256.0);
             Test(12345);
         }
-
-        [TestMethod]
-        public void Add_Int32()
-        {
-            void Test(int x, int y) => Assert.AreEqual(x + y, ElementaryFunctions.Add(x, y));
-
-            Test(6, 9);
-            Test(15, 17);
-            Test(-3, 5);
-            Test(3, -5);
-            Test(-3, -5);
-            Test(0xFFFF, 1);
-            Test(8, int.MaxValue);
-        }
-
-        [TestMethod]
-        public void Subtract_Int32()
-        {
-            void Test(int x, int y) => Assert.AreEqual(x - y, ElementaryFunctions.Subtract(x, y));
-
-            Test(6, 9);
-            Test(5, 3);
-            Test(-3, 5);
-            Test(3, -5);
-            Test(-3, -5);
-            Test(0xFFFF, -1);
-            Test(8, int.MaxValue);
-        }
-
-        [TestMethod]
-        public void Multiply_Byte()
-        {
-            void Test(byte x, byte y) => Assert.AreEqual((byte)(x * y), ElementaryFunctions.Multiply(x, y));
-
-            Test(6, 5);
-            Test(15, 17);
-            Test(8, 32);
-            Test(51, 53);
-        }
-
-        [TestMethod]
-        public void Multiply_Int32()
-        {
-            void Test(int x, int y) => Assert.AreEqual(x * y, ElementaryFunctions.Multiply(x, y));
-
-            Test(6, 5);
-            Test(15, 17);
-            Test(-3, 5);
-            Test(3, -5);
-            Test(-3, -5);
-            Test(8, int.MaxValue);
-            Test(0x10000000, -4);
-        }
     }
 }
